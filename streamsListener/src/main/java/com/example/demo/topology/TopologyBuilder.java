bdc7c3fa3f2011ada89fba6c449d93e19f5d8aeb
k.puda <krzysztof.puda@ing.com>
2022-04-15 12:31:14 +0200

    added stateful operation

--- streamsListener/src/main/java/com/example/demo/topology/TopologyBuilder.java
+++ streamsListener/src/main/java/com/example/demo/topology/TopologyBuilder.java
@@ -2,13 +2,13 @@ package com.example.demo.topology;
 
 import com.example.demo.model.Incident;
 import com.example.demo.streams.CustomSerde;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -24,30 +24,36 @@ public class TopologyBuilder {
     public Topology createTopology() {
         String validMessagesTopic = "validMessages";
         String invalidMessagesTopic = "invalidMessages";
-        String pizzaOrderTopic = "pizzaOrder";
+        String pizzaTopic = "pizzaOrder";
         String incidentTopic = "pineapplePizzaTopic";
 
         StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-        //valid messages
-        KStream<String, String> validStream = streamsBuilder.stream(validMessagesTopic, Consumed.with(Serdes.String(), Serdes.String()));
-        validStream.peek((key,messages)-> log.info("Valid message: {}",messages));
+        //CREATE STREAM FOR VALID MESSAGES
+        KStream<String, String> validStream = streamsBuilder.<String, String>stream(validMessagesTopic);
+        validStream.peek((key,messages)->log.info("Valid message received: {}",messages));
 
-        //invalid messages
-        KStream<String, String> invalidStream = streamsBuilder.stream(invalidMessagesTopic, Consumed.with(Serdes.String(), Serdes.String()));
-        invalidStream.peek((key,messages)-> log.info("Inalid message: {}",messages));
+        //CREATE STREAM FOR INVALID MESSAGES
+        KStream<String, String> invalidStream = streamsBuilder.<String, String>stream(invalidMessagesTopic);
+        invalidStream.peek((key,messages)->log.info("Invalid message received: {}",messages));
 
-        //order stream
-        KStream<Object, Object> pizzaMap = streamsBuilder.stream(pizzaOrderTopic);
-        pizzaMap.peek((key,value)->log.info("New order: \n {}",value));
 
-        //incident
+        //CREATE STREAM FOR PIZZA ORDERS
+        KStream<String, String> pizzaStream = streamsBuilder.<String, String>stream(pizzaTopic);
+        pizzaStream.peek((key,value)->log.info("{}",value));
+
+        //CREATE STREAM FOR INCIDENTS
         CustomSerde<Incident> incSerde= new CustomSerde<>(Incident.class);
-        KStream<String, Incident> incMap = streamsBuilder.stream(incidentTopic, Consumed.with(Serdes.String(), incSerde));
-        incMap.peek((key,value)-> log.info("{}", value));
+        KStream<String, Incident> incStream = streamsBuilder.stream(incidentTopic, Consumed.with(Serdes.String(), incSerde));
+        incStream.peek((key,value)->log.info("New incident: {}",value));
 
-        //Stateful operations
-
+        //CREATE STREAM FOR STATEFUL OPERATIONS
+        Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
+        KTable<String, Long> kTable = validStream.flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+                .groupBy((key, word) -> word)
+                .count();
+        kTable.toStream()
+                .foreach((word, count) -> System.out.println("word: " + word + " -> " + count));
         return streamsBuilder.build();
     }
 }
